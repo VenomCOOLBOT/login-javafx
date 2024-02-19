ca46f5903722f4d194c6c06e5128a77008cf04bf
Vernon Pitney <vernon@pitney.us>
2024-02-19 09:39:20 -0800

    First commit

---
+++ login-demo/src/main/java/com/example/App.java
@@ -0,0 +1,29 @@
+package com.example;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("login.fxml"));
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setScene(new Scene(root, 520, 400));
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ login-demo/src/main/java/com/example/LoginController.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class LoginController {
+    @FXML
+    private Button cancelButton;
+
+    @FXML
+    private Label loginMessageLabel;
+
+    @FXML
+    private TextField usernameTextField;
+
+    @FXML
+    private PasswordField passwordTextField;
+
+    public void loginButtonOnAction(ActionEvent event) {
+        
+
+        if(usernameTextField.getText().isBlank() == false && passwordTextField.getText().isBlank() == false) {
+            validateLogin();
+        } else {
+            loginMessageLabel.setText("Please enter username and password");
+        }
+    }
+
+    public void cancelButtonOnAction(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+
+    public void validateLogin() {
+        // This for validation of username and password in database
+    }
+}
